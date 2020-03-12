e778657f9baeefce3e32bb2f18f4ac27346f5055
bocman2003 <legitmanxx557@gmail.com>
2020-03-12 20:46:39 +0100

    Add files via upload

--- macchanger.py
+++ macchanger.py
@@ -1,28 +1,48 @@
+
 import subprocess as sb
 import random
 
+def randmac():
+   mac = ""
+   counter = 0
+   while True:
+      if counter == 6:
+         return mac
+      cislo = str(random.randrange(10, 99))
+      if counter == 5:
+         mac = mac + cislo
+      else:
+         mac = mac + cislo + ":"
+      counter += 1
 
+def macchange():
+   sb.call(f"ifconfig {interface} down ", shell=True)
+   sb.call(f"ifconfig {interface} hw ether {mac}", shell=True)
+   sb.call(f"ifconfig {interface} up", shell=True)
 
-interface = input("interface u wanna change mac on:\n")
-mac = str(input("type a mac or random(r):\n"))
+interface = str(input("specify interface > "))
+mac = str(input("type MAC or r for random MAC > "))
 
 #random mac gen
 if mac == "r":
-    mac = ""
-    counter = 0
-    while True:
-        if counter == 6:
-            break
-        cislo = str(random.randrange(10,99))
-        if counter == 5:
-            mac = mac + cislo
-        else:
-            mac = mac + cislo + ":"
-        counter += 1
-
-print(f"mac = {mac}")
+   mac = randmac()
 
 
-sb.call(f"ifconfig {interface} down ", shell=True)
-sb.call(f"ifconfig {interface} hw ether {mac}", shell=True)
-sb.call(f"ifconfig {interface} up", shell=True)
+
+print(f"[+] changing MAC to > {mac}")
+macchange()
+
+
+#checks for the correct MAC
+ifconfig_result = str(sb.check_output(f"ifconfig {interface}", shell=True))
+if (mac in ifconfig_result) == True:
+
+   print("[+] MAC changed!")
+
+
+
+
+
+
+
+
